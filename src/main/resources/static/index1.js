542aebd8038b873530e88438003762c21967aad7
Vyshnavi Kurapati <vyshnavikurapati609@gmail.com>
2026-07-18 12:14:11 +0530

    toggleuser changes

--- src/main/resources/static/index1.js
+++ src/main/resources/static/index1.js
@@ -1,2 +1,28 @@
 // alert("hello ,meet");
 console.log("hello from js")
+var users= [
+    {
+         "name": "jane doe",
+        "gender": "Female",
+        "img": "jane.png"
+    },
+    {
+        "name": "john doe",
+        "gender": "Male",
+        "img": "john.png"
+    }
+]
+
+var id= 0;
+function toggleUser(){
+    id=(id+1)%2
+
+var userImage=document.getElementById("user-image");
+userImage.src=users[id].img;
+
+var userName=document.getElementById("user-name");
+userName.innerHTML= users[id].name;
+
+var userGender= document.getElementById("user-gender");
+userGender.innerHTML= users[id].gender;
+}
