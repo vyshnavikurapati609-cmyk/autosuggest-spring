17ffdd9ba14b22a54e4f5d25c4802f298303336c
Vyshnavi Kurapati <vyshnavikurapati609@gmail.com>
2026-07-18 12:50:20 +0530

    randomUser change

--- src/main/resources/static/index1.js
+++ src/main/resources/static/index1.js
@@ -26,3 +26,25 @@ userName.innerHTML= users[id].name;
 var userGender= document.getElementById("user-gender");
 userGender.innerHTML= users[id].gender;
 }
+function randomUser(){
+    fetch("https://randomuser.me/api/")
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data)
+{
+    var userData= data.results[0];
+    var userImage=document.getElementById("user-image");
+    userImage.src=userData.picture.large;
+
+    var userName=document.getElementById("user-name");
+    userName.innerHTML= userData.name.first +" "+ userData.name.last;
+
+    var userGender= document.getElementById("user-gender");
+    userGender.innerHTML= userData.gender;
+    
+})
+.catch(function(err){
+    console.log(err);
+})
+}
